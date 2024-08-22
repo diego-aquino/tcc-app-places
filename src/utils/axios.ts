a2c4af313887d2771593dd4dec2e731d01a3668f
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-08-22 14:42:03 -0300

    refactor: format response status with interpolation

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import { AxiosError, AxiosResponse } from 'axios';
 
 export function createAxiosErrorFromResponse(response: AxiosResponse) {
   return new AxiosError(
-    'Request failed with status code ' + response.status,
+    `Request failed with status code ${response.status}`,
     response.statusText,
     response.config,
     response.request,
